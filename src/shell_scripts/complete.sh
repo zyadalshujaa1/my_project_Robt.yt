cfedc9d439b58d1c67a1f6952e3ed1309fb2d38d
Zyad Alshuja <91574172+zyadalshujaa1@users.noreply.github.com>
2025-09-16 20:02:17 +0300

    Delete src/shell_scripts directory

--- src/shell_scripts/complete.sh
+++
@@ -1,8 +0,0 @@
-cd /home/shadow1/zetaxbot
-source devel/setup.sh
-export ROS_MASTER_URI=http://localhost:11311
-#export ROS_IP=192.168.45.227
-#export ROS_HOSTNAME=192.168.45.227
-
-roslaunch zetaxbot_bringup complete.launch
-
